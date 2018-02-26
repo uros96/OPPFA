82874095f7a0da16e170db40db0a8dabf0fac050
uros96 <34546412+uros96@users.noreply.github.com>
2018-02-26 17:16:51 +0100

    update

--- vezba01/zad7.py
+++ vezba01/zad7.py
@@ -1,10 +1,10 @@
 import sys
 
 if __name__=="__main__":
-    t1=(1, 0.1, "one")
-    t2=(2, 0.2, "two")
-    t3=(3, 0.3, "three")
-    t4=(4, 0.4, "four")
+    t1=(1234, 1.234, "first")
+    t2=(2134, 12.34, "second")
+    t3=(3124, 123.4, "third")
+    t4=(4123, 1234.0, "fourth")
     s={t1, t2, t3, t4};
     print(s)
     print('\n')
